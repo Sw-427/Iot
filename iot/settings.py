42bbba02c03d71bfb54c1043f872d6c532df639f
Sw-427 <74399335+Sw-427@users.noreply.github.com>
2022-11-01 01:29:17 +0530

    Update settings.py

--- iot/settings.py
+++ iot/settings.py
@@ -131,7 +131,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR/'staticfilep'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
